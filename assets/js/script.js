d60e8f26e2609abc50338df0f080bc05f47e5e55
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-04-08 01:26:55 -0400

    added jquery and bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// api.openweathermap.org/data/2.5/forecast?lat=43.836915001007554&lon=-79.28722704405882&appid=a28b4bca4758cee1be1ac1e4e61612a1
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=a28b4bca4758cee1be1ac1e4e61612a1
+// 43.836915001007554, -79.28722704405882;
